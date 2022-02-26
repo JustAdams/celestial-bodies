fe5c40be4bd46e45238bb89410cb2c5420ffa104
Justin Adams <AdamsJustin@protonmail.com>
2022-02-26 10:45:54 -0500

    PlayerController
    Moved camera movement to the Player to fix movement issues

--- CelestialBodies/Assets/Scripts/CameraController.cs
+++ CelestialBodies/Assets/Scripts/CameraController.cs
@@ -7,6 +7,7 @@ public class CameraController : MonoBehaviour
     public float mouseSensitivity = 100f;
     private float xRotation = 0f;
 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +17,6 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move Y axis  
-        var verticalMovement = (Input.GetKey(KeyCode.Space) ? 1 : 0) + (Input.GetKey(KeyCode.C) ? -1 : 0);
-        // Move X and Z axis
-        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), verticalMovement, Input.GetAxis("Vertical"));
-        transform.Translate(movement, transform);
-
         // Mouse look
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
---
+++ CelestialBodies/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move Y axis  
+        var verticalMovement = (Input.GetKey(KeyCode.Space) ? 1 : 0) + (Input.GetKey(KeyCode.C) ? -1 : 0);
+        // Move X and Z axis
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), verticalMovement, Input.GetAxis("Vertical"));
+        transform.Translate(movement, transform);
+    }
+}
